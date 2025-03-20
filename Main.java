f3446f4c8289bda4b4192bd970d7a5121fe83bca
Hariprasath006 <harinataraj2005@gmail.com>
2025-03-20 15:26:21 +0530

    Update Main.C

--- Main.java
+++ Main.java
@@ -1,41 +1,25 @@
-import java.util.Scanner;
-
-class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter size: ");
-        int size = sc.nextInt();
-
-        int[] numbers = new int[size + 2]; 
-
-        
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < size; i++) {
-            numbers[i] = sc.nextInt();
-        }
-
-        
-        System.out.print("Enter begin: ");
-        int begin = sc.nextInt();
-        for (int i = size; i > 0; i--) {
-            numbers[i] = numbers[i - 1];
-        }
-        numbers[0] = begin;
-        size++;
-
-        
-        System.out.print("Enter end: ");
-        int end = sc.nextInt();
-        numbers[size] = end;
-        size++;
-
-       
-        System.out.print("New array: ");
-        for (int i = 0; i < size; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-
-        sc.close();
+#include<stdio.h>
+int main()
+{
+    int size,i,begin,end;
+    printf("Enter size:");
+    scanf("%d",&size);
+    int numbers[size + 2];
+    for(int i=0;i<size;i++){
+        scanf("%d",&numbers[i]);
     }
+    printf("enter begin:");
+    scanf("%d",&begin);
+    for(int i=size;i>0;i--){
+        numbers[i]=numbers[i-1];
+    }
+    numbers[0]=begin;
+    printf("enter end:");
+    scanf("%d",&end);
+    numbers[size + 1]=end;
+    printf("new array:");
+    for(int i=0;i<=size + 1;i++){
+        printf("%d ", numbers[i]);
+    }
+    return 0;
 }
